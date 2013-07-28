a32e13a1197daf7f3a74593e7d9347de8e78aa20
boris.puzikov <boris.puzikov@gmail.com>
2013-07-28 19:42:14 +0000

    added timechart

--- src/main/java/ImageCreator.java
+++ src/main/java/ImageCreator.java
@@ -2,11 +2,15 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.time.Year;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +25,39 @@ public class ImageCreator {
     private static final String yAxis = "";
 
     public void create(Team team) {
+        createXYChart(team);
+        createTimeChart(team);
+    }
+
+    private void createTimeChart(Team team) {
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+
+        for (Map line : team.getData()) {
+            TimeSeries series = new TimeSeries(line.toString());
+            Iterator i = line.entrySet().iterator();
+            while (i.hasNext()) {
+                Map.Entry<Integer, Integer> pair = (Map.Entry) i.next();
+                series.add(new Year(pair.getKey()), pair.getValue());
+            }
+            dataset.addSeries(series);
+        }
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+                team.getName(),
+                xAxis,
+                yAxis,
+                dataset,
+                false,                      // Show Legend
+                false,                      // Use tooltips
+                false                       // Configure chart to generate URLs?
+        );
+        try {
+            ChartUtilities.saveChartAsJPEG(new File("time" + team.getName() + ".jpg"), chart, 500, 300);
+        } catch (IOException e) {
+            System.err.println("Problem occurred creating chart.");
+        }
+    }
+
+    private void createXYChart(Team team) {
         XYSeriesCollection dataset = new XYSeriesCollection();
 
         for (Map line : team.getData()) {
@@ -44,10 +81,11 @@ public class ImageCreator {
                 false                       // Configure chart to generate URLs?
         );
         try {
-            ChartUtilities.saveChartAsJPEG(new File(team.getName() + ".jpg"), chart, 500, 300);
+            ChartUtilities.saveChartAsJPEG(new File("xy" + team.getName() + ".jpg"), chart, 500, 300);
         } catch (IOException e) {
             System.err.println("Problem occurred creating chart.");
         }
     }
+
 }
 
