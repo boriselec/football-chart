e75d05b01119c212ce02dc51c3fc08ac06f29781
boris.puzikov <boris.puzikov@gmail.com>
2013-08-03 13:12:13 +0000

    added visible dot shapes; fixed tick unit;

--- src/main/java/ImageCreator.java
+++ src/main/java/ImageCreator.java
@@ -1,14 +1,19 @@
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.axis.DateTickUnit;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.time.Year;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -20,6 +25,8 @@ import java.util.Map;
 public class ImageCreator {
     private static final String xAxis = "";
     private static final String yAxis = "";
+    private static final Color mainColor = Color.RED;
+    private static final Shape dotShape = new Rectangle2D.Double(-3, -3, 6, 6);
 
     public void create(Team team) {
         createTimeChart(team);
@@ -47,9 +54,17 @@ public class ImageCreator {
                 false                       // Configure chart to generate URLs?
         );
         XYPlot plot = (XYPlot) chart.getPlot();
-        for (int i = 0; i < dataset.getSeriesCount(); i++)
-            plot.getRenderer().setSeriesPaint(i, Color.RED);
+        XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) plot.getRenderer();
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        axis.setTickUnit(new DateTickUnit(DateTickUnit.YEAR, 1, new SimpleDateFormat("yyyy")));
+        plot.getDomainAxis().setVerticalTickLabels(true);
         plot.getRangeAxis().setInverted(true);
+
+        for (int i = 0; i < dataset.getSeriesCount(); i++) {
+            renderer.setSeriesPaint(i, mainColor);
+            renderer.setSeriesShapesVisible(i, true);
+            renderer.setSeriesShape(i, dotShape);
+        }
 
         try {
             ChartUtilities.saveChartAsJPEG(new File("time" + team.getName() + ".jpg"), chart, 500, 300);
