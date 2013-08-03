bff84a44e08973384a63a95e548802ecfbba9c52
boris.puzikov <boris.puzikov@gmail.com>
2013-08-03 15:02:01 +0000

    added visible dot shapes; fixed tick unit; added rpl stub for global structure; added stats parser; fixed image creation;

--- src/main/java/GraphCreator.java
+++ src/main/java/GraphCreator.java
@@ -1,5 +1,6 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Author: Boris Puzikov
@@ -7,29 +8,13 @@ import java.util.Map;
  * Time: 9:48 PM
  */
 public class GraphCreator {
-    public static void main(String... args) {
-        //test
-        Map line1 = new HashMap<Integer, Integer>();
-        line1.put(1946, 1);
-        line1.put(1947, 4);
-        line1.put(1948, 9);
-        line1.put(1949, 1);
-        Map line2 = new HashMap<Integer, Integer>();
-        line2.put(1966, 1);
-        line2.put(1967, 5);
-        line2.put(1968, 9);
-        line2.put(1969, 1);
-        Map line3 = new HashMap<Integer, Integer>();
-        line3.put(1976, 1);
-        line3.put(1977, 5);
-        line3.put(1978, 9);
-        line3.put(1979, 1);
-        Team team = new Team("Dynamo");
-        team.addData(line1);
-        team.addData(line2);
-        team.addData(line3);
+    public static void main(String... args) throws IOException {
+        StatsLoader loader = new StatsLoader();
+        String currentPath = System.getProperty("user.dir");
+        List<Team> teams = loader.load(new File(currentPath + "/stats/"));
 
         ImageCreator imageCreator = new ImageCreator();
-        imageCreator.create(team);
+        for (Team team : teams)
+            imageCreator.create(team, team.getEnName());
     }
 }
